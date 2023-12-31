fc9ff4e1d0f1241fa5bb27670ba8672d756b0208
Varun Khatri <varun.h.khatri@gmail.com>
2023-12-31 13:07:43 +0530

    User Registration Implementation

---
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -0,0 +1,5 @@
+export 'model/model.dart';
+export 'dto/dto.dart';
+export 'utils/logger.dart';
+export 'implementation.dart';
+export 'exceptions/exceptions.dart';
---
+++ packages/authentication_repository/lib/implementation.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+/// Repository Implementation for User Registration, Login and Logout.
+class AuthenticationRepository {
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final String _apiUrl;
+
+  AuthenticationRepository({
+    required firebase_auth.FirebaseAuth firebaseAuth,
+    required String apiUrl,
+  })  : _firebaseAuth = firebaseAuth,
+        _apiUrl = apiUrl;
+
+  /// Validate HTTP requests and check for errors.
+  /// If errors exist, throw the appropriate error.
+  void _validateRequestAndThrowError({
+    required http.Response response,
+    required String errorLogName,
+  }) {
+    // Check for 4XX errors and throw errors accordingly
+    if (response.statusCode >= 400 && response.statusCode < 500) {
+      Map<String, dynamic> body = json.decode(response.body);
+      throw ServerException(message: body['message']);
+    }
+
+    // Check for internal server error, log it and throw a server exception.
+    else if (response.statusCode >= 500) {
+      Map<String, dynamic> body = json.decode(response.body);
+      log.e("$errorLogName Error", error: body['message']);
+      throw ServerException(
+        message: 'Something went wrong, please try again later.',
+      );
+    }
+  }
+
+  /// Register a new account using email and password.
+  Future<Credentials> registerWithEmailAndPassword({
+    required String emailAddress,
+    required String password,
+  }) async {
+    // Prepare URI for the request.
+    Uri uri = Uri.parse("$_apiUrl/doctor/specialization");
+
+    // Prepare authorization headers.
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+    };
+
+    // Send the post request to the server.
+    http.Response response = await http.post(
+      uri,
+      headers: headers,
+    );
+
+    // Check for any errors.
+    _validateRequestAndThrowError(
+      response: response,
+      errorLogName: "AuthenticationRepository:registerWithEmailAndPassword",
+    );
+
+    // Decode JSON and create object based on it.
+    dynamic jsonResponse = json.decode(response.body);
+    Credentials credentials = Credentials.fromJson(jsonResponse);
+
+    // Return credentials.
+    return credentials;
+  }
+}
