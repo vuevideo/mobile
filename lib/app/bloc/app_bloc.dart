371ecaee287d4922fd65030bdad696e271b83c12
Varun Khatri <varun.h.khatri@gmail.com>
2024-01-07 16:19:24 +0530

    Comments

--- lib/app/bloc/app_bloc.dart
+++ lib/app/bloc/app_bloc.dart
@@ -7,27 +7,40 @@ import 'package:bloc/bloc.dart';
 part 'app_event.dart';
 part 'app_state.dart';
 
+/// Bloc Implementation for App wide Authentication State.
 class AppBloc extends Bloc<AppEvent, AppState> {
+
+  // Authentication repository and Firebase user stream.
   final AuthenticationRepository _authenticationRepository;
   late final StreamSubscription<Future<Credentials>> _credentialsSubscription;
 
+  // Bloc constructor
   AppBloc({required AuthenticationRepository authenticationRepository})
       : _authenticationRepository = authenticationRepository,
+
+  // Initial state to use.
         super(
           authenticationRepository.currentUser.isEmpty()
               ? const AppState.unauthenticated()
               : AppState.authenticated(authenticationRepository.currentUser),
         ) {
+
+    // Handling events and their corresponding state changes.
     on<AppAuthChanged>(_onUserChanged);
     on<AppLogoutRequested>(_onLogoutRequested);
 
+    // Subscribing to the firebase user stream.
     _credentialsSubscription =
         _authenticationRepository.user.listen((credentials) async {
       add(AppAuthChanged(credentials: await credentials));
     });
   }
 
+  /// Handle Authentication Changes Events.
   void _onUserChanged(AppAuthChanged event, Emitter<AppState> emit) {
+
+    // Check if user is empty, if yes then emit authenticated state else
+    // emit unauthenticated state.
     emit(
       event.credentials.isEmpty()
           ? const AppState.unauthenticated()
@@ -35,6 +48,7 @@ class AppBloc extends Bloc<AppEvent, AppState> {
     );
   }
 
+  /// Handle Authentication Login Events.
   void _onLogoutRequested(AppLogoutRequested event, Emitter<AppState> emit) {
     unawaited(_authenticationRepository.logOut());
   }
