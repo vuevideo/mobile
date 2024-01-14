5c0284bb8f710c2a5fe9feef2e67e6a761be96ca
Varun Khatri <varun.h.khatri@gmail.com>
2024-01-14 14:29:40 +0530

    Folder Structure Redo

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:mobile/app/app.dart';
+import 'package:mobile/features/app/app.dart';
 
 class App extends StatelessWidget {
   final AuthenticationRepository authenticationRepository;
