b2356e11a5f8e5e47f426b87001265e90cba9a02
Varun Khatri <varun.h.khatri@gmail.com>
2024-01-14 17:28:49 +0530

    App Routing (Need to look into Flow Builder)

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flow_builder/flow_builder.dart';
 
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:mobile/features/app/app.dart';
@@ -35,23 +36,28 @@ class AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'VueVideo Mobile',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.greenAccent,
         ),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'VueVideo'),
+      home: FlowBuilder<AppStatus>(
+        state: context.select(
+          (AppBloc bloc) => bloc.state.appStatus,
+        ),
+        onGeneratePages: onGeneratedAppViewPages,
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+  static Page<void> page() => const MaterialPage<void>(child: HomePage());
 
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -71,7 +77,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("VueVideo"),
       ),
       body: Center(
         child: Column(
