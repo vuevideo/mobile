8b4c7031d4da051e9f599862d320afc712613005
Varun Khatri <varun.h.khatri@gmail.com>
2024-01-14 14:30:20 +0530

    Form Fields Validation Added

---
+++ lib/models/email.dart
@@ -0,0 +1,25 @@
+import 'package:formz/formz.dart';
+
+/// Email Validation Error
+enum EmailValidationError {
+  /// Generic invalid error.
+  invalid
+}
+
+/// Form input for an Email input.
+class Email extends FormzInput<String, EmailValidationError> {
+  const Email.pure() : super.pure('');
+
+  const Email.dirty([super.value = '']) : super.dirty();
+
+  static final RegExp _emailRegExp = RegExp(
+    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
+  );
+
+  @override
+  EmailValidationError? validator(String? value) {
+    return _emailRegExp.hasMatch(value ?? '')
+        ? null
+        : EmailValidationError.invalid;
+  }
+}
---
+++ lib/models/models.dart
@@ -0,0 +1,4 @@
+export './email.dart';
+export './name.dart';
+export './password.dart';
+export './username.dart';
