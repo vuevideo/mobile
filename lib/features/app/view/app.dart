ab64261f9da48444015f6fdab32020cd9ed80806
Varun Khatri <varun.h.khatri@gmail.com>
2024-01-28 18:25:11 +0530

    Router Overhaul

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flow_builder/flow_builder.dart';
 
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:mobile/features/app/app.dart';
@@ -35,7 +34,7 @@ class AppView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'VueVideo Mobile',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -43,61 +42,8 @@ class AppView extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: FlowBuilder<AppStatus>(
-        state: context.select(
-          (AppBloc bloc) => bloc.state.appStatus,
-        ),
-        onGeneratePages: onGeneratedAppViewPages,
-      ),
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  static Page<void> page() => const MaterialPage<void>(child: HomePage());
-
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("VueVideo"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
